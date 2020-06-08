8b606024926dd21e581f355ca80c6bf548739b26
John Curry <fastpenguin91@gmail.com>
2020-06-07 17:58:30 -0700

    ready to start jQuery part

---
+++ new-plugin.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Plugin Name: new plugin
+ * Plugin URI: https://truthseekers.io
+ * Description: registering scripts styles and stuff
+ * Author: John
+ * Author URI: https://truthseekers.io
+ */
+function new_plugin_setup()
+{
+    $asset_file = include(plugin_dir_path(__FILE__) . 'build/index.asset.php');
+
+    wp_register_script(
+        'truth-new-plugin-script',
+        plugins_url('build/index.js', __FILE__),
+        $asset_file['dependencies'],
+        $asset_file['version']
+    );
+
+    wp_register_style(
+        'truth-new-block-style',
+        plugins_url('/src/style.css', __FILE__),
+        array('wp-edit-blocks')
+    );
+
+    register_block_type('truth/new-plugin-block', array(
+        'editor_script' => 'truth-new-plugin-script',
+        'editor_style'  => 'truth-new-block-style'
+    ));
+}
+
+add_action('init', 'new_plugin_setup');
---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log("hello from new-plugin js lala");
+import { registerBlockType } from '@wordpress/blocks';
+// import './style.css';
+
+registerBlockType('truth/new-plugin-block', {
+    title: 'new plugin block',
+    icon: 'smiley',
+    category: 'layout',
+    edit: ({ className }) => <div className={className}>I am the edit test</div>,
+    save: () => <div>I am the frontend test!!</div>,
+});
